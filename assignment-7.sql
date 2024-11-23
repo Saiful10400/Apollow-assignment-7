39778249ce3fde80b5b037de7d24853b4820ac64
saiful10400 <saifulislam10400i5@gmail.com>
2024-11-23 12:31:32 +0600

    data insertion done.

--- assignment-7.sql
+++ assignment-7.sql
@@ -22,5 +22,44 @@ CREATE Table courses (
 -- ............enrolement table............
 CREATE Table enrollment (
     enrollment_id SERIAL PRIMARY KEY,
-    student_id
+    student_id INTEGER REFERENCES students(student_id) on delete CASCADE,
+    course_id INTEGER REFERENCES courses(course_id) on delete CASCADE
 )
+
+
+
+
+
+-- inserting Data........
+
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+
+
+INSERT INTO courses (course_name, credits)
+VALUES
+('Next.js', 3),
+('React.js', 4),
+('Databases', 3),
+('Prisma', 3);
+
+
+INSERT INTO enrollment (student_id, course_id)
+VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2);
+
+
+
+SELECT * FROM enrollment
+JOIN students USING(student_id)
+JOIN courses USING(course_id)
